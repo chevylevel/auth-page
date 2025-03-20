c2bb784a7732976f99009fe6df6087697420d71f
chevylevel <chevylevel@gmail.com>
2025-03-20 11:52:10 +0300

    refactor names

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,16 @@ import React from "react";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import { ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material';
+import themeSettings from './theme';
 
-export const theme = createTheme({
-  typography: {
-    fontFamily: "'Inter', sans-serif",
-  },
-  breakpoints: {
-    values: {
-      mobile: 0,
-      tablet: 768,
-      laptop: 1440,
-      desktop: 1920,
-    },
-  },
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#526ED3',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-  }
-});
+export const theme = createTheme(themeSettings);
 
 function App() {
-  return <ThemeProvider theme={theme}>
-    <RegisterPage />
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <RegisterPage />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+export default {
+    typography: {
+        fontFamily: "'Inter', sans-serif",
+    },
+    breakpoints: {
+        values: {
+            mobile: 0,
+            tablet: 768,
+            laptop: 1440,
+            desktop: 1920,
+        },
+    },
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#526ED3',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+    }
+}
